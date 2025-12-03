c1373230de3106bff6b525cadc54a1c39e2da27d
wtasg <225132696+wtasg@users.noreply.github.com>
2025-12-03 21:59:41 +0530

    blah blah

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,21 @@ import { TicTacToe } from './showcase/tic-tac-toe/TicTacToe'
 function App() {
     return (
         <>
-            <h1>Hi!</h1>
-            <p>This is my showcase for react apps.</p>
-            <div style={{ height: "100px", width: "content", margin: "auto" }}>
-                <a href="https://github.com/wtasg/meetonline">MeetOnline App Repo</a>
+            <div className="left-side-bar">
+                <a href="/tic-tac-toe">Tic Tac Toe</a>
             </div>
-            <TicTacToe />
+            <main role="main">
+                <section>
+                    <h1>Hi!</h1>
+                    <p>This is my showcase for react apps.</p>
+                    <div style={{ height: "100px", width: "content", margin: "auto" }}>
+                        <a href="https://github.com/wtasg/meetonline">MeetOnline App Repo</a>
+                    </div>
+                </section>
+                <section>
+                    <TicTacToe />
+                </section>
+            </main>
         </>
     )
 }
