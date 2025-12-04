9c930ebf3f7ab5d1693d0be05d4a6d843664a36c
wtasg <225132696+wtasg@users.noreply.github.com>
2025-12-04 22:00:06 +0530

    working game, and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
+import { useState } from 'react'
 import './App.css'
 import { TicTacToe } from './showcase/tic-tac-toe/TicTacToe'
 
 function App() {
+    const [path, setPath] = useState(localStorage.getItem("route") || "/");
+
+    function changeRoute(route: string) {
+        setPath(route);
+        localStorage.setItem("route", route);
+    }
+
     return (
-        <>
-            <div className="left-side-bar">
-                <a href="/tic-tac-toe">Tic Tac Toe</a>
-            </div>
+        <div className="App">
+            <nav>
+                <button onClick={() => changeRoute("/")}>Home</button>
+                <button onClick={() => changeRoute("/tic-tac-toe")}>Tic Tac Toe</button>
+            </nav>
             <main role="main">
-                <section>
-                    <h1>Hi!</h1>
-                    <p>This is my showcase for react apps.</p>
-                    <div style={{ height: "100px", width: "content", margin: "auto" }}>
-                        <a href="https://github.com/wtasg/meetonline">MeetOnline App Repo</a>
-                    </div>
-                </section>
-                <section>
-                    <TicTacToe />
-                </section>
+                {
+                    path === "/" &&
+                    <section>
+                        <h1>Hi!</h1>
+                        <p>This is my showcase for react apps.</p>
+                        <div style={{ height: "100px", width: "content", margin: "auto" }}>
+                            <a href="https://github.com/wtasg/meetonline">MeetOnline App Repo</a>
+                        </div>
+                    </section>
+                }
+                {
+                    path === "/tic-tac-toe" &&
+
+                    <section>
+                        <TicTacToe />
+                    </section>
+                }
             </main>
-        </>
+        </div >
     )
 }
 
--- src/showcase/tic-tac-toe/TicTacToe.tsx
+++ src/showcase/tic-tac-toe/TicTacToe.tsx
@@ -1,77 +1,102 @@
 import { useState } from "react";
+import "./TicTacToe.css"
 
-type Cell = {
-    value: string;
-}
-
-type Row = Cell[];
-
-type State = {
-    turn: string;
-    board: Row[];
-};
+const initialSquares = (): ("X" | "O" | "")[] => Array(9).fill(0).map(() => "");
 
 function TicTacToe() {
-    const initialState: State = {
-        turn: "X",
-        board: [
-            [{ value: "X" }, { value: "O" }, { value: " " },],
-            [{ value: " " }, { value: " " }, { value: " " },],
-            [{ value: " " }, { value: " " }, { value: " " },],
-        ]
-    };
+    const [squares, setSquares] = useState<("X" | "O" | "")[]>(initialSquares());
+    const [turn, setTurn] = useState<"X" | "O">("X");
+    const [gameState, setGameState] = useState<"over" | "playing" | "tied">("playing");
+    const [playerWon, setPlayerWon] = useState<"X" | "O" | "">("");
+    const [wincom, setWincom] = useState<number[]>([]);
+    const [moves, setMoves] = useState(9);
 
-    const [state, setState] = useState(initialState);
-
-    return <DrawBoard state={state} />;
-}
-
-function DrawBoard({ state }: { state: State }) {
-    return <>
-        <div>Turn: {state.turn}</div>
-        <Board board={state.board} />
-    </>
-}
-
-function Board({ board }: { board: Row[] }) {
-    const boardStyle = { display: "grid", gridGap: "10px", border: "thin solid red", padding: "10px" };
-    return <div style={boardStyle}>
-        {
-            board.map((row: Row, rowIndex: number) => {
-                return <Row row={row} index={rowIndex} />
-            })
+    function onSquareClick(index: number) {
+        if (squares[index] !== "" || gameState === "over" || playerWon !== "" || gameState === "tied") {
+            return;
         }
+        const newMoves = moves - 1;
+        setMoves(newMoves);
+        const newSquares = [...squares.slice(0, index), turn, ...squares.slice(index + 1)];
+        setSquares(newSquares);
+        setTurn(turn === "X" ? "O" : "X");
+        const { won, combination } = winner(newSquares);
+        if (won === "X" || won === "O") {
+            setWincom(combination);
+            setGameState("over");
+            setPlayerWon(won);
+            setTurn("X");
+            return;
+        }
+        if (newMoves === 0) {
+            setGameState("tied");
+            return;
+        }
+    }
+
+    function reset() {
+        setTurn("X");
+        setSquares(initialSquares());
+        setPlayerWon("");
+        setGameState("playing");
+        setWincom([]);
+    }
+
+    return <div className="game">
+        <h1>Game: Tic Tac Toe</h1>
+        <div className="board">
+            {
+                squares
+                    .map(
+                        (val: string, index: number) =>
+                            <Square
+                                index={index}
+                                val={val}
+                                onSquareClick={onSquareClick}
+                                klass={playerWon && wincom.includes(index) ? "wincom" : ""}
+                            />
+                    )
+            }
+        </div>
+        <div className="state">
+            <button className="reset" onClick={reset}>reset</button>
+            {playerWon && <div className="won">{playerWon}</div>}
+            {gameState === "over" && <div className="over">
+                Game Over!
+            </div>
+            }
+            {gameState === "tied" && <div>Tied</div>}
+        </div>
     </div>
 }
 
-function Row({ row, index }: { row: Row, index: number }) {
-    const rowStyle = { display: "grid", gridGap: "10px", gridTemplateColumns: "1fr 1fr 1fr", };
-
+function Square({ index, val, onSquareClick, klass }: { index: number, val: string, onSquareClick: (index: number) => void, klass: string }) {
     return <div
-        key={`board-row-${index}`}
-        style={rowStyle}>
-        {row.map((cell: Cell, cellIndex: number) => {
-            return <Cell index={cellIndex} value={cell.value} />
-        })}
+        className={`square ${val === "" ? "empty" : "filled"} ${klass}`}
+        key={`square_${index}`}
+        onClick={() => onSquareClick(index)}
+    >
+        {val}
     </div>
 }
 
-function Cell({ index, value }: { index: number, value: string }) {
-    const cellStyle = {
-        height: "100px",
-        width: "100px",
-        border: "thin solid yellow",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "48px",
-        color: "white"
-    };
-    return <div
-        key={`board-cell-${index}`}
-        style={cellStyle}>
-        {value}
-    </div >
+function winner(squares: ("X" | "O" | "")[]): { won: "X" | "O" | "", combination: number[] } {
+    const winningRows = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    const winningColumns = [[0, 3, 6], [1, 4, 7], [2, 5, 8]];
+    const diagonals = [[0, 4, 8], [2, 4, 6]];
+    const winningCombinations = [
+        ...winningRows,
+        ...winningColumns,
+        ...diagonals
+    ];
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const curr = winningCombinations[i];
+        const [ai, bi, ci] = curr;
+        if (squares[ai] === squares[bi] && squares[bi] === squares[ci] && squares[ci] !== "") {
+            return { won: squares[ai], combination: [ai, bi, ci] };
+        }
+    }
+    return { won: "", combination: [] };
 }
 
 export {
